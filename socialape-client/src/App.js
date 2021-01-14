390df31904ccfdd15c62fe925f414c0967701a5f
cardi <graytraces@gmail.com>
2021-01-14 23:03:20 +0900

    Getting Started With React 완료

--- socialape-client/src/App.js
+++ socialape-client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import Navbar from "./component/Navbar";
+
+//Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home}></Route>
+              <Route exact path="/login" component={login}></Route>
+              <Route exact path="/signup" component={signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ socialape-client/src/component/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Link from "react-router-dom/Link";
+//{} 쓰면 전체를 로딩하니 그러지 말자
+
+class Navbar extends Component {
+  render() {
+    return (
+      <AppBar>
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/signup">
+            Signup
+          </Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Navbar;
