63c6313654bb0bbf7bfdc6b76c2ab48271307fd6
graytraces <graytraces@gmail.com>
2021-01-16 07:35:36 +0900

    Login form 10분까지 진행

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+
+import { Grid } from "@material-ui/core";
+const styles = {
+  form: {
+    textAlign: "center",
+  },
+};
 
 class login extends Component {
-    render() {
-        return (
-            <div>
-                login
-            </div>
-        )
-    }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm></Grid>
+        <Grid item sm>
+          <p>Yoooo</p>
+        </Grid>
+        <Grid item sm></Grid>
+      </Grid>
+    );
+  }
 }
 
-export default login
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
