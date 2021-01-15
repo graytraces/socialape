f95be5eb932c684874ed31050518dfcfad5a137e
cardi <graytraces@gmail.com>
2021-01-16 08:29:18 +0900

    Login Form 완료

--- socialape-client/src/pages/login.js
+++ socialape-client/src/pages/login.js
@@ -1,23 +1,147 @@
 import React, { Component } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import PropTypes from "prop-types";
+import AppIcon from "../images/favicon.ico";
+import axios from "axios";
 
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  CircularProgress,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
 const styles = {
   form: {
     textAlign: "center",
   },
+  image: {
+    margin: "20px auto 20px auto",
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {
+    margin: "10px auto 10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position:"relative"
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progress:{
+      position:"absolute"
+  }
 };
 
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post("/login", userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
     const { classes } = this.props;
+    const { errors, loading } = this.state;
 
     return (
       <Grid container className={classes.form}>
         <Grid item sm></Grid>
         <Grid item sm>
-          <p>Yoooo</p>
+          <img src={AppIcon} alt="bloomstick" className={classes.image} />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.TextField}
+              value={this.state.email}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.TextField}
+              value={this.state.password}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}
+              disabled={loading}
+            >
+              Login
+              {loading && <CircularProgress className={classes.progress} size={30} />}
+            </Button>
+            <br />
+            <small>
+              don't have an account ? signup <Link to="/signup"> here </Link>
+            </small>
+          </form>
         </Grid>
         <Grid item sm></Grid>
       </Grid>
